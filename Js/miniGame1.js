614d4522313b3f89f972cbda8ac7d6a2dd2b5c23
coding-salmon <ejzus88@naver.com>
2024-03-22 18:18:36 +0900

    홀드합치기 게임중

--- Js/miniGame1.js
+++ Js/miniGame1.js
@@ -21,15 +21,35 @@ function startGame1() {
                 }
             },
             scene: {
-                // preload: preload, 오디오 주석처리
+                preload: preload, 
                 create: create1,
-                // update: update
+                update: update
         }
     };
 
     var game1 = new Phaser.Game(config1);
 } 
 
+function preload(){
+    //홀드 이미지 로드
+    this.load.image("rock1", "../miniGameImages/hold1.jpg");
+    this.load.image("rock2", "../miniGameImages/hold2.jpg");
+    this.load.image("rock3", "../miniGameImages/hold3.jpg");
+
+}
+
 function create1() {
+    //홀드 물리 객체 생성
+    this.rock = this.physics.add.image(400, 0, "rock");
+    this.rock =setCollideWorldBounds(true); //화면 경계와 충돌처리
+    this.rock = setBounce(0.5); //돌이 바운스되는 정도 설정
+    this.rock = setInteractive(); //돌이 인터렉티브하도록 설정
+
+    //과일이 드래그해서 위치 조절 가능하게 함
+    this.input.setDraggable(this.rock);
+    this.input.on("drag",function (pointer,gameObject,dragX,dragY){
+        gameObject.x=dragX;
+    });
+
     
 }
